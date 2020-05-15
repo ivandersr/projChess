e710de1ce86a95458d6757a06b172c149cc1eb50
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2020-05-15 10:51:59 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,12 +29,12 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
-	}	
+	}
 
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
@@ -46,8 +46,7 @@ public class UI {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
 		}
 	}
-		
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -63,12 +62,12 @@ public class UI {
 		if (piece == null) {
 			System.out.print("-");
 		} else {
-			if (piece.getColor() == Color.WHITE) { 
+			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
 			} else {
 				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
 			}
-				
+
 		}
 		System.out.print(" ");
 	}
