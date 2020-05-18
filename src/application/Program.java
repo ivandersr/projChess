38774fd698156ecdaa811154f4551337e536d89e
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2020-05-18 12:40:24 -0300

    Special move - Castling

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,7 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
+		
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
